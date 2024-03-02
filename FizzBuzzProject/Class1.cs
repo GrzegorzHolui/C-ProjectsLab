970fb8d726c1a790b1751d96a22591f42e3c123a
grzes <grzesiu.holui@gmail.com>
2024-03-02 09:24:15 +0100

    game fizzbuzz

---
+++ FizzBuzzProject/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace FizzBuzzProject
+{
+    class FizzBuzz
+    {
+        int amount;
+        public FizzBuzz(int amount)
+        {
+            this.amount = amount; // Set the initial value for model
+        }
+
+        public void showFizzBuzz()
+        {
+            for (int i = 1; i <= amount; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
+
--- FizzBuzzProject/Program.cs
+++ FizzBuzzProject/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzzProject;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizz = new FizzBuzz(15);
+            fizz.showFizzBuzz();
+        }
+    }
+}
