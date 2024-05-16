9fc7db9aabe16c8ddd5ab86fed372c18e16792a4
grzes <grzesiu.holui@gmail.com>
2024-05-16 16:22:33 +0200

    add zadanie 2

---
+++ Lab4/BlazorAppLaby2/BlazorAppLaby2/Components/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAppLaby2.Components
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public DateTime? ReleaseDate { get; set; }
+		public float? Rate { get; set; }
+		public string? ImageUrl { get; set; }
+	}
+
+}
---
+++ Lab4/BlazorAppLaby2/BlazorAppLaby2/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlazorAppLaby2.Components;
+
+namespace BlazorAppLaby2.Data
+{
+	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+	{
+	    public DbSet<BlazorAppLaby2.Components.Movie> Movie { get; set; } = default!;
+	}
+}
